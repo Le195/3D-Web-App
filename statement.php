239524763afb85df8e5bcbc20353467d1451d111
Le195 <xule76890@gmail.com>
2023-05-09 22:35:42 +0100

    modified:   .DS_Store modified:   statement.php

--- statement.php
+++ statement.php
@@ -17,6 +17,6 @@
 	<h1>Statement of Originality</h1>
 	<p>"These web pages are submitted as part requirement for the degree of Computer Science and Artificial Intelligence at the University of Sussex. They are the product of my own labour except where indicated in the web page content. These web pages or contents may be freely copied and distributed provided the source is acknowledged".</p>
 	<p style="text-align: left;">LE XU 2023</p>
-	<a href="index.php" class="btn btn-style btn-secondary mt-lg-5 mt-4 me-2">Click it to back home👆 </a>
+	<a href="about.php" class="btn btn-style btn-secondary mt-lg-5 mt-4 me-2">Click it to back home👆 </a>
 </body>
 </html>
