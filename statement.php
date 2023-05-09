d03bc6dc711a26e361c905884c4d3a5a16da2424
Le <133051322+Le195@users.noreply.github.com>
2023-05-09 21:48:06 +0100

    Add files via upload

---
+++ statement.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Statement of Originality</title>
+	<style>
+		body {
+			font-size: 18px;
+			text-align: center;
+		}
+		h1 {
+			font-size: 32px;
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+	<h1>Statement of Originality</h1>
+	<p>"These web pages are submitted as part requirement for the degree of Computer Science and Artificial Intelligence at the University of Sussex. They are the product of my own labour except where indicated in the web page content. These web pages or contents may be freely copied and distributed provided the source is acknowledged".</p>
+	<p style="text-align: left;">LE XU 2023</p>
+	<a href="index.php" class="btn btn-style btn-secondary mt-lg-5 mt-4 me-2">Click it to back home👆 </a>
+</body>
+</html>
